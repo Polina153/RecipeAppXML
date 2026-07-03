e0307d1327b09ab6204894ad1039a8ce0f7c6d05
Linka <Polina153@mail.ru>
2026-07-04 01:06:03 +0400

    Все замечания учтены

--- app/src/main/java/com/example/recipeappxml/MainActivity.kt
+++ app/src/main/java/com/example/recipeappxml/MainActivity.kt
@@ -9,12 +9,13 @@ import com.example.recipeappxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
